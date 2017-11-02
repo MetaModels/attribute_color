77ce5e1aeb0a2c0f460fff6a518f511078da5ee4
David Molineus <david.molineus@netzmacht.de>
2017-11-02 15:12:27 +0100

    Add base bundle structure.

---
+++ tests/Test/DependencyInjection/MetaModelsAttributeColorExtensionTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * * This file is part of MetaModels/attribute_color.
+ *
+ * (c) 2012-2017 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage AttributeColor
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2012-2017 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_text/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels\Test\Attribute\Color\DependencyInjection;
+
+use MetaModels\Attribute\Color\AttributeTypeFactory;
+use MetaModels\Attribute\Color\DependencyInjection\MetaModelsAttributeColorExtension;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+/**
+ * This test case test the extension.
+ */
+class MetaModelsAttributeColorExtensionTest extends TestCase
+{
+    /**
+     * Test that extension can be instantiated.
+     *
+     * @return void
+     */
+    public function testInstantiation()
+    {
+        $extension = new MetaModelsAttributeColorExtension();
+
+        $this->assertInstanceOf(MetaModelsAttributeColorExtension::class, $extension);
+        $this->assertInstanceOf(ExtensionInterface::class, $extension);
+    }
+
+    /**
+     * Test that the services are loaded.
+     *
+     * @return void
+     */
+    public function testFactoryIsRegistered()
+    {
+        $container = $this->getMockBuilder(ContainerBuilder::class)->getMock();
+
+        $container
+            ->expects($this->once())
+            ->method('setDefinition')
+            ->with(
+                'metamodels.attribute_color.factory',
+                $this->callback(
+                    function ($value) {
+                        /** @var Definition $value */
+                        $this->assertInstanceOf(Definition::class, $value);
+                        $this->assertEquals(AttributeTypeFactory::class, $value->getClass());
+                        $this->assertCount(1, $value->getTag('metamodels.attribute_factory'));
+
+                        return true;
+                    }
+                )
+            );
+
+        $extension = new MetaModelsAttributeColorExtension();
+        $extension->load([], $container);
+    }
+}
