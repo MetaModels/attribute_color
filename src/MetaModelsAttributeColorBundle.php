d20007a48fe56a7b12e04ad36662d9f00f9a4410
Christian Schiffler <c.schiffler@cyberspectrum.de>
2019-01-07 15:27:17 +0100

    Fix author validation

--- src/MetaModelsAttributeColorBundle.php
+++ src/MetaModelsAttributeColorBundle.php
@@ -3,22 +3,18 @@
 /**
  * * This file is part of MetaModels/attribute_color.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage AttributeColor
+ * @package    MetaModels/attribute_color
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @author     Andreas Isaak <info@andreas-isaak.de>
- * @author     Stefan Heimes <stefan_heimes@hotmail.com>
- * @author     Cliff Parnitzky <github@cliff-parnitzky.de>
- * @copyright  2012-2017 The MetaModels team.
- * @license    https://github.com/MetaModels/attribute_text/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_color/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
